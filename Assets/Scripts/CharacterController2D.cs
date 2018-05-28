0da10e769abdbcc6f4bca6e1a878c9ae5bd72809
excelo <excel.ortega@gmail.com>
2018-05-27 18:59:32 -0700

    Project Start

---
+++ Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Handles input and movement of a character
+[ExecuteInEditMode]
+public class CharacterController2D : MonoBehaviour 
+{
+
+	public float speed;
+	public float rayLength;
+	[Range(0.1f, 1.25f)]
+	public float boundsize;
+	[Range(2, 10)]
+	public int raycount;
+	private Rigidbody2D rb;
+	private BoxCollider2D col;
+
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody2D>();
+		col = GetComponent<BoxCollider2D>();
+		Debug.Log(col.bounds.max);
+	}
+	
+	void Update () 
+	{
+		Move();
+
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, rayLength);
+
+		float stepy = col.bounds.size.y / (raycount-1);
+		for (float i = 0; i <= col.bounds.size.y; i += stepy)
+		{
+			RaycastHit2D currenthit = Physics2D.Raycast(transform.position + new Vector3(col.bounds.extents.x, col.bounds.extents.y - i), Vector3.right, rayLength);
+			Debug.DrawRay(transform.position + new Vector3(col.bounds.extents.x, col.bounds.extents.y - i, 0) * boundsize, Vector3.right * rayLength, Color.red);
+			if (currenthit.collider != null && currenthit.collider.tag != "Player")
+				Debug.Log(currenthit.collider.gameObject.name);
+		}
+		float stepx = col.bounds.size.x / (raycount-1);
+		for (float i = 0; i <= col.bounds.size.x; i += stepx)
+		{
+			Debug.DrawRay(transform.position - new Vector3(col.bounds.extents.x - i, col.bounds.extents.y, 0) * boundsize, Vector3.down * rayLength, Color.red);
+		}
+	}
+
+	void FixedUpdate()
+	{
+		
+	}
+
+	void Move()
+	{
+		rb.MovePosition(rb.position + new Vector2(1, 0) * speed * Input.GetAxis("Horizontal") * Time.deltaTime);	
+	}
+
+	void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireCube(transform.position, new Vector3(col.bounds.size.x * boundsize, col.bounds.size.y * boundsize, 0));
+	}
+}
---
+++ Assets/Scripts/DrawColliders.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class DrawColliders : MonoBehaviour 
+{
+	Collider2D[] colliders;
+
+	void Start()
+	{
+		colliders = GetComponents<Collider2D>();
+	}
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.yellow;
+		foreach (Collider2D col in colliders)
+		{
+			if (col.GetType().Equals(typeof(BoxCollider2D)))
+			{
+				Gizmos.DrawWireCube(transform.position, col.bounds.size);
+			}
+			else if (col.GetType().Equals(typeof(CircleCollider2D)))
+			{
+				CircleCollider2D temp = (CircleCollider2D)col;
+				Gizmos.DrawWireSphere(transform.position, temp.radius);
+			}
+		}
+	}
+}
